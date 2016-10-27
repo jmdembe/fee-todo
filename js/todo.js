d71ea38533bf3604c1b08d96c788ca05e54ca93c
Jessica Dembe <jmdembe@gmail.com>
2016-10-27 16:32:42 -0400

    Created function that processes select and prevents default action

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,10 @@
-(function ( {
+(function() {
 
     document
         .querySelector('form')
-        .addEventListener('submit', function storeListItem(event) {
+        .addEventListener('submit', function storeListItem(event) {   //submit button to store the list item
             event.preventDefault();
             console.log("HELLO!");
         });
 
-
-})();
+}());
