50b250778d90f942d04c637f478698d9e7babbf2
Jessica Dembe <jmdembe@gmail.com>
2016-10-28 14:20:06 -0400

    New list items now appear after submission

--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,23 @@
 (function() {
 
-    document
+    document //submit feature
         .querySelector('form')
         .addEventListener('submit', function storeListItem(event) {   //submit button to store the list item
-            event.preventDefault();
-            console.log("HELLO!");
+          event.preventDefault();
+
+          var item = document.querySelector('.new-todo');
+          console.log(item.value);
+          addListItem(item.value);
         });
 
-    function addListItem(text) {;
+    function addListItem(text) {    //adds list items upon entry
         var listItem = document.createElement('li');
         listItem.innerText = text;
         document.querySelector('ul.items').appendChild(listItem);
     }
+
+
+
 
 
 }());
