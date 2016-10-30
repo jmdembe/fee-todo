178a71886fd389d8540fa59ee1d443c725e02ff6
Jessica Dembe <jmdembe@gmail.com>
2016-10-30 16:16:26 -0400

    Added code which successfully adds the completed class to completed list item

--- js/data.js
+++ js/data.js
@@ -1,16 +1,42 @@
 (function (){
     if (typeof(window.arrayList) !== 'object') {
-      window.fee = [];
+      window.arrayList = [];
+    }
 
-      var item = document.querySelector('.new-todo');
-      addListItem(item.value);
 
-      var itemList = {
-          text: item.value,
-          time: Date.now()
-      };
+      document //submit feature
+          .querySelector('form')
+          .addEventListener('submit', function storeListItem(event) {   //submit button to store the list item
+            event.preventDefault();
 
-      arrayList.push(itemList);
-      console.log(arrayList);
+            var item = document.querySelector('.new-todo');
+            addListItem(item.value);
+
+            var itemList = {
+                text: item.value,
+                time: Date.now()
+            };
+        });
+
+        function addListItem(text) {    //adds list items upon entry
+            var listItem = document.createElement('li');
+            var addArticle = document.createElement('article');
+
+            var checkButton = document.createElement('button');
+            checkButton.classList.add('check');
+            addArticle.appendChild(checkButton);
+
+            var addP = document.createElement('p');
+            addP.innerText = text;
+            addArticle.appendChild(addP);
+
+            var deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete');
+            deleteButton.innerText = 'X';
+            addArticle.appendChild(deleteButton);
+
+            listItem.appendChild(addArticle);
+            document.querySelector('ul.items').appendChild(listItem);
+          }
 
 })();
--- js/todo.js
+++ js/todo.js
@@ -3,32 +3,17 @@
     window.arrayList = window.arrayList || [];
     var counter = 0 ;
 
-    function addListItem(text) {    //adds list items upon entry
-        var listItem = document.createElement('li');
-        var addArticle = document.createElement('article');
+    document
+        .querySelector('.items')
+        .addEventListener('click', function completedItem (event) {
+              if (event.target.tagName === 'BUTTON' && event.target.className === 'check' ) {
+                  console.log("LOOK AT ME!");
+                  event.target.parentNode.classList.add('completed');
 
-        var checkButton = document.createElement('button');
-        checkButton.classList.add('check');
-        addArticle.appendChild(checkButton);
+              }
 
-        var addP = document.createElement('p');
-        addP.innerText = text;
-        addArticle.appendChild(addP);
+        });
+      console.log(arrayList);
 
-        var deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete');
-        deleteButton.innerText = 'X';
-        addArticle.appendChild(deleteButton);
-
-        listItem.appendChild(addArticle);
-        document.querySelector('ul.items').appendChild(listItem);
-      }
-
-      document
-          .querySelector('.items')
-          .addEventListener('click', function completedItem (event) {
-
-
-          });
 
 })();
