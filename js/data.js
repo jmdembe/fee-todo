dcb3fa00d21f375062a232f7ee743c82e589255a
Jessica Dembe <jmdembe@gmail.com>
2016-10-31 19:45:44 -0400

    Added refactored code after in-class review

--- js/data.js
+++ js/data.js
@@ -1,48 +1,27 @@
 (function (){
-    window.arrayProperties = window.arrayProperties || {};
+    window.todos = window.todos || {};
 
       var toDoList = [];
-      var counter = 0;
+      var incompleteCounter = 0;
 
-      document //submit feature
-          .querySelector('form')
-          .addEventListener('submit', function storeListItem(event) {   //submit button to store the list item
-            event.preventDefault();
 
-            var item = document.querySelector('.new-todo');
-            addListItem(item.value);
-            counter++;
+      window.todos.addTodoData = function addTodoData(text) {
+        incompleteCounter++;
 
-            var itemList = {
-                text: item.value,
-                time: Date.now()
-            };
+        var itemList = {
+            text: text,
+            time: Date.now()
+        };
 
-            toDoList.push(itemList);
-            arrayProperties[counter] = counter;
-            arrayProperties[itemList] = itemList;
+        toDoList.push(itemList);
+      }
 
-        });
+      window.todos.updateCounter = function updateCounter(change) {
+        incompleteCounter += change;
+      }
 
-        function addListItem(text) {    //adds list items upon entry
-            var listItem = document.createElement('li');
-            var addArticle = document.createElement('article');
-
-            var checkButton = document.createElement('button');
-            checkButton.classList.add('check');
-            addArticle.appendChild(checkButton);
-
-            var addP = document.createElement('p');
-            addP.innerText = text;
-            addArticle.appendChild(addP);
-
-            var deleteButton = document.createElement('button');
-            deleteButton.classList.add('delete');
-            deleteButton.innerText = 'X';
-            addArticle.appendChild(deleteButton);
-
-            listItem.appendChild(addArticle);
-            document.querySelector('ul.items').appendChild(listItem);
-          }
+      window.todos.getCounter = function getCounter() {
+        return incompleteCounter;
+      }
 
 })();
--- js/todo.js
+++ js/todo.js
@@ -1,19 +1,70 @@
 (function() {
 
-    window.arrayProperties = window.arrayProperties || {};
+    window.todos = window.todos || {};
+
+
+    document //submit feature
+        .querySelector('form')
+        .addEventListener('submit', function storeListItem(event) {   //submit button to store the list item
+          event.preventDefault();
+
+          var item = document.querySelector('.new-todo');
+          addListItem(item.value);
+
+          window.todos.addTodoData(item.value);
+
+          updateIncompleteCounterUI();
+
+      });
+
+      function addListItem(text) {    //adds list items upon entry
+          var listItem = document.createElement('li');
+          var addArticle = document.createElement('article');
+
+          var checkButton = document.createElement('button');
+          checkButton.classList.add('check');
+          addArticle.appendChild(checkButton);
+
+          var addP = document.createElement('p');
+          addP.innerText = text;
+          addArticle.appendChild(addP);
+
+          var deleteButton = document.createElement('button');
+          deleteButton.classList.add('delete');
+          deleteButton.innerText = 'X';
+          addArticle.appendChild(deleteButton);
+
+          listItem.appendChild(addArticle);
+          document.querySelector('ul.items').appendChild(listItem);
+        }
 
     document
         .querySelector('.items')
         .addEventListener('click', function itemStatus (event) {
               if (event.target.tagName === 'BUTTON' && event.target.className === 'check' ) {
                   console.log("LOOK AT ME!");
-                  event.target.parentNode.classList.add('completed');
+
+                  if (event.target.parentNode.className === 'completed') {
+                      event.target.parentNode.classList.remove('completed');
+                      window.todos.updateCounter(1);
+                  } else {
+                      event.target.parentNode.classList.add('completed');
+                      window.todos.updateCounter(-1);
+                  }
+                  updateIncompleteCounterUI();
+
               }
 
               if (event.target.tagName === 'BUTTON' && event.target.className === 'delete') {
-                  event.target.parentNode.removeChild('article');
+                  document.querySelector('.items').removeChild(event.target.parentNode.parentNode);
+                  // update the counter!!
+                  // also, can we remove that object from our data array??
               }
         });
+
+        function updateIncompleteCounterUI() {
+          document.querySelector('.incomplete-items').innerText = window.todos.getCounter();
+        }
 
 
 })();
